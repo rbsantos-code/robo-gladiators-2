fe61e205e2d45d1af466e1836bc7d7adeebf11ca
rbsantos-code <richardbriansantos91@yahoo.com>
2021-08-17 20:46:17 -0700

    set up player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+
+let playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert('The fight has begun!');
+}
+
+//fight();
