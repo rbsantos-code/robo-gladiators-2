d93bf05d1074e5a2a60040d4805d1c7fa4d9af2e
rbsantos-code <richardbriansantos91@yahoo.com>
2021-08-20 21:48:58 -0700

    attack js

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,21 @@ let enemyAttack = 12;
 
 function fight() {
     window.alert("Welcome to Robot Gladiators!");
+
+    // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+
+    enemyHealth = enemyHealth - playerAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
+
+    // Log a resulting message to the console so we know that it worked.
 };
 
+// execute function
 fight();
