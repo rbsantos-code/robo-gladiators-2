892f4baa613ad4343680dcd0527b9693b9e4a904
rbsantos-code <richardbriansantos91@yahoo.com>
2021-08-20 21:09:47 -0700

    console logging name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 
 let playerName = window.prompt("What is your robot's name?");
 console.log(playerName);
+console.log("Our robot's name is " + playerName);
 
 // this creates a function named "fight"
 
